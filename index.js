831520a09ac7a54c062aa3e8bedb05400427043c
ChrisK106 <christian_k106@hotmail.com>
2024-03-14 01:39:04 -0500

    Agregado método GET /listar-videos
    Se agrega el método GET /listar-videos para generar una respuesta JSON que contiene información de las clases grabadas en Q10

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import express, { response } from 'express';
+import express from 'express';
 import axios from 'axios';
 import * as cheerio from 'cheerio';
 import cors from 'cors'
@@ -7,6 +7,8 @@ const app = express()
 const port = process.env.PORT || 8000;
 const domainUrl = 'https://site.q10.com'
 const loginUrl = domainUrl + '/User/Login?returnUrl=%2F'
+const loginUser = 'chris.ae.cgca@gmail.com'
+const loginPassword = 'SMjutz215'
 
 app.use(express.static('public'))
 
@@ -36,8 +38,8 @@ app.get('/notas/:matriculaId/periodo/:periodoId', (req, res) => {
     })
     
     axiosInstance.post(loginUrl, {
-        NombreUsuario: 'chris.ae.cgca@gmail.com',
-        Contrasena: 'SMjutz215'
+        NombreUsuario: loginUser,
+        Contrasena: loginPassword
         })
     .then(response => {
         html = response.data
@@ -152,6 +154,114 @@ app.get('/notas/:matriculaId/periodo/:periodoId', (req, res) => {
                 })
             })
         })
+    })
+    .catch( error => { console.log(error) })
+})
+
+app.get('/listar-videos', (req, res) => {
+    
+    let html, $
+    
+    // OW AplentId
+    let aplentId = '3d708335-480c-41dc-b2cc-07d79f3b4eab'
+    // OW UserId
+    let userId = '81232'
+    // OW Username
+    let username = 'p.castillo.f%40orsonwelles.edu.pe'
+    // OW PersonaId
+    let personaId = '112619872678'
+    
+    let authUrl = domainUrl + '/AutenticarUsuario?roleId=0&aplent=' + aplentId + '&codigoUsuario=' + userId + '&userName=' + username + '&recordarme=False&codigoPersona=' + personaId
+
+    const axiosInstance = axios.create({
+        withCredentials: true,
+        headers: {
+            "Content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+        },
+    })
+    
+    axiosInstance.post(authUrl)
+    .then(async response => {
+        axiosInstance.interceptors.request.use(config => {
+            config.headers['Cookie'] = response.headers['set-cookie'];
+            return config;
+        })
+
+        let data = []
+        let recordedClassesListUrl = domainUrl + '/AulasVirtuales/ClasesGrabadas/Lista?pagina='
+
+        //get page range from query params or default to 1-20
+        let pageFrom = req.query.from || 1
+        let pageTo = req.query.to || 20
+        //check if hasSubjectId flag is set to true or default to false
+        let hasSubjectIdFlag = req.query.hasSubjectId === 'true'
+
+        console.log('Página desde: ' + pageFrom + ' hasta: ' + pageTo)
+        
+        for (let i = pageFrom; i <= pageTo; i++) {
+            await axiosInstance.get(recordedClassesListUrl + i)
+            .then(async response => {
+                
+                //load response
+                html = response.data
+                $ = cheerio.load(html)
+
+                //check if there are no results
+                if($('.no-results').text().trim() === 'No hay registros') return
+
+                //get all recorded classes in current page
+                let recs = $('body > div > div > div.table-responsive > table > tbody > tr')
+                
+                let date, subjectId, subjectName, teacher, roomId
+                
+                //iterate over recorded classes (up to 12 per page)
+                for (let rec of recs) {
+
+                    console.log('Página ' + i + ' - Rec ' + (recs.index(rec) + 1) + ' de ' + recs.length)
+
+                    date = $('td:eq(0)', $(rec)).text().trim()
+                    subjectId = $('td:eq(1)', $(rec)).text().trim()
+                    subjectName = $('td:eq(2)', $(rec)).text().trim()
+                    teacher = $('td:eq(3)', $(rec)).text().trim()
+                    roomId = $('td:eq(4)', $(rec)).text().trim()
+                    
+                    //check if subjectId is empty and hasSubjectIdFlag is true to skip current class
+                    if (hasSubjectIdFlag && subjectId === '') continue
+
+                    let videos = []
+
+                    //check if class has video recordings
+                    if ($('td:eq(5)', $(rec)).text().trim() !== 'La clase virtual no se llevó a cabo') {
+
+                        //get video recordings of current class
+                        let vids = $('body > div > div > div.table-responsive > table > tbody > tr:nth-child(' + (recs.index(rec) + 1) + ') > td:nth-child(6) > table > tbody > tr')
+
+                        let info, audit, url, downloadUrl
+
+                        //iterate over video recordings
+                        for (let vid of vids) {
+                            info = $(vid).find('span').attr('title')
+                            audit = $(vid).find('i').attr('title')
+                            url = $(vid).find('a').attr('data-relative')
+
+                            if (url !== undefined) {
+                                await axiosInstance.get(domainUrl + '/Archivo/AzureStorage/GetFileUrl?url=' + url)
+                                .then(response => {
+                                    downloadUrl = response.data
+                                })
+                            }
+                            videos.push({ info, audit, url, downloadUrl })
+                        }
+                    }
+
+                    //check if videos array is not empty to push it to data array
+                    if (videos.length > 0) {
+                        data.push({ date, subjectId, subjectName, teacher, roomId, videos })
+                    }
+                }
+            })
+        }
+        res.json(data)
     })
     .catch( error => { console.log(error) })
 })
